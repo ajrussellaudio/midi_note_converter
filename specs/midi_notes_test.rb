efc7c3527e9a4ef5c63531cd7e914158637263f4
Alan Russell <ajrussellaudio@gmail.com>
2016-07-19 21:35:33 +0100

    adds tests

---
+++ specs/midi_notes_test.rb
@@ -0,0 +1,22 @@
+require "minitest/autorun"
+require "minitest/rg"
+require_relative "../midi_notes"
+
+class Functions_Practice < MiniTest::Test
+
+  def test_middle_c()
+    middle_c_result = note_name(60)
+    assert_equal( "C3", middle_c_result )
+  end
+
+  def test_lowest_c()
+    lowest_c_result = note_name(0)
+    assert_equal( "C-2", lowest_c_result )
+  end
+
+  def test_sharp()
+    f_sharp_result = note_name(66)
+    assert_equal( "F#3", f_sharp_result )
+  end
+
+end
